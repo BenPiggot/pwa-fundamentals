b7507fd3a4925045611a8c8a237ff52d34190d06
BPiggot <bpiggot@logicworks.net>
2018-07-23 16:20:04 -0700

    Basic caching

--- client/service-worker.js
+++ client/service-worker.js
@@ -1,11 +1,45 @@
+const FALLBACK_IMAGE_URL = 'https://localhost:3100/images/fallback-grocery.png';
+const fallbackImages = 'fallback-images';
+
 self.addEventListener('install', event => {
-  console.log("I'm installing");
+  event.waitUntil(
+    caches.open(fallbackImages).then(cache => {
+      return cache.add(FALLBACK_IMAGE_URL)
+    })
+  )
 });
 
 self.addEventListener('activate', event => {
-  console.log("I'm alive!");
+  caches.keys().then(cacheNames => {
+    
+  })
 });
 
+function fetchImageOrFallback(fetchEvent) {
+  console.log(fetchEvent)
+  return fetch(fetchEvent.request.url, {
+    mode: 'cors'
+  }).then((response) => {
+    if (!response.ok) {
+      return caches.match(FALLBACK_IMAGE_URL, { cacheName: fallbackImages });
+    }
+    else {
+      return response;
+    }
+  }).catch(() => {
+    return caches.match(FALLBACK_IMAGE_URL, { cacheName: fallbackImages });
+  })
+}
+
 self.addEventListener('fetch', event => {
-  console.log('Someone tried to fetch something');
+  let acceptHeader = event.request.headers.get('accept');
+  let requestUrl = new URL(event.request.url);
+  let isGroceryImage = acceptHeader.indexOf('image/*') >= 0 && requestUrl.pathname.indexOf('/images/') === 0;
+
+  if (acceptHeader && isGroceryImage) {
+    console.log('image fetch')
+    event.respondWith(
+      fetchImageOrFallback(event)
+    );
+  }
 }); 
